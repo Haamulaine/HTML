8da0fb9c17ecc053b740dc46d921636e53f5a2ac
Haamulaine <eemeli.ea.laine@gmail.com>
2025-10-06 13:55:36 +0300

    P15.js

---
+++ JS/P16.js
@@ -0,0 +1,4 @@
+function nelionAla(sivu) {
+    console.log(sivu * sivu);
+}
+//nelionala(2);
---
+++ JS/P17.js
@@ -0,0 +1,4 @@
+function suorakulmionAla(sivu1, sivu2) {
+    console.log(sivu1 * sivu2);
+}
+//suorakulmionAla(2, 3);

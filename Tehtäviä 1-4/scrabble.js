06c7ec107ef684a997a5214259c160d21f263d8b
Haamulaine <eemeli.ea.laine@gmail.com>
2025-11-12 20:22:44 +0200

    Tehtäviä 1-4

--- Tehtäviä 1-4/scrabble.js
+++ Tehtäviä 1-4/scrabble.js
@@ -1,3 +1,24 @@
 function tarkastaPisteet() {
+    const sana = document.getElementById("sana").value.toUpperCase();
+    const pisteet = {
+        A: 1, E: 1, I: 1, N: 1, S: 1, T: 1,
+        O: 2, Ä: 2, K: 2, L: 2,
+        U: 3, M: 3,
+        Y: 4, H: 4, J: 4, P: 4, R: 4, V: 4,
+        Ö: 7,  D: 7,
+        B: 8, F: 8, G: 8,
+        C: 10
+    };
 
+    let summa = 0;
+    for (let kirjain of sana) {
+        summa += pisteet[kirjain] || 12;
+    }
+
+    const tulos = document.getElementById("tulos");
+    if (sana.length === 0) {
+        tulos.textContent = "Syötä luku ensin.";
+    } else {
+        tulos.textContent = `Sanan "${sana}" pistemäärä on ${summa} pistettä.`;
+    }
 }
